4a2cc3ea26ed3736845ead0739fc32190289962e
Torkin1 <daniele.laprova@hotmail.it>
2023-08-12 14:52:49 +0200

    registering block device driver and obtaining major number

--- bldms.c
+++ bldms.c
@@ -1,22 +1,53 @@
-// TODO: write a makefile to compile this baby
-
 #define __KERNEL__
 #define MODULE
+
+#define DEBUG
 
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/printk.h>
+#include <linux/genhd.h>
+
+/** Name of driver*/
+const char *bldms_name = "bldms";
+
+enum bldms_errors {
+    BLDMS_E_OK = 0,
+    BLDMS_E_NOMAJOR = -1, 
+} bldms_errors;
+
+/**
+ * Gets a Major number selected by the kernel
+ * among the available ones
+*/
+static int bldms_getMajorNumber(void){
+    int major = register_blkdev(0, bldms_name);
+    if(major < 0){
+        pr_err("%s: register_blkdev returned error code %d\n", __func__, major);
+    }
+    return major;
+}
 
 static int bldms_init(void){
 
-    pr_info("Hello, world\n");
+    int major;
+    
+    pr_devel("Loading bldms module\n");
+
+    /** Register driver by acquiring a major*/
+    major = bldms_getMajorNumber();
+    if (major < 0){
+        pr_err("unable to get major number\n");
+        return major;
+    }
+    pr_devel("Got major number %d\n", major);
     
     return 0;
 }
 
 static void bldms_exit(void){
 
-    pr_info("Goodbye, world\n");
+    pr_devel("Goodbye, world\n");
 }
 
 module_init(bldms_init);
